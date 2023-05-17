836ac63dffa1fdc8d5c599b7a2adbd33b75ee4bc
LautyFarías247 <lauty0618@gmail.com>
2023-05-17 17:46:07 -0300

    failed register

--- cypress/e2e/5-failed-register.cy.js
+++ cypress/e2e/5-failed-register.cy.js
@@ -1,12 +1,12 @@
 describe("Registro con email existente", () => {
-  it("Debe lanzar un error",()=>{
-		cy.visit("https://automationexercise.com/")
-		cy.get('.active > :nth-child(1) > h1 > span')
-		cy.get('.shop-menu > .nav > :nth-child(4) > a').click()
-		cy.get('.signup-form > h2').contains("New User Signup!")
-		cy.get('[data-qa="signup-name"]').type("prueba")
-		cy.get('[data-qa="signup-email"]').type("usuariodeprueba@prueba")
-		cy.get('[data-qa="signup-button"]').click()
-		cy.get('.signup-form > form > p').contains("Email Address already exist!")
-	});
+  it("Debe lanzar un error", () => {
+    cy.visit("https://automationexercise.com/");
+    cy.get(".active > :nth-child(1) > h1 > span");
+    cy.get(".shop-menu > .nav > :nth-child(4) > a").click();
+    cy.get(".signup-form > h2").contains("New User Signup!");
+    cy.get('[data-qa="signup-name"]').type("prueba");
+    cy.get('[data-qa="signup-email"]').type("usuariodeprueba@prueba");
+    cy.get('[data-qa="signup-button"]').click();
+    cy.get(".signup-form > form > p").contains("Email Address already exist!");
+  });
 });
