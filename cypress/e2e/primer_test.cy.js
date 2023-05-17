bb7c305a3c5b7bbfce3cd26b39ba1aa49e3389fc
LautyFarías247 <lauty0618@gmail.com>
2023-05-16 23:05:49 -0300

    primer test

---
+++ cypress/e2e/primer_test.cy.js
@@ -0,0 +1,11 @@
+describe("Primera tanda de test de sumas ", () => {
+  const two = 2;
+  const dos = 2;
+  it("2+2 es cuatro(4)", () => {
+    expect(dos + two).to.equal(4);
+  });
+
+  it("el resultado es true", () => {
+		expect(true).to.equal(true)
+	});
+});
